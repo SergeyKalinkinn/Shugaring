08d6e8adb94cc987f8b5db7236b681800928d967
Sergey <571030@mail.ru>
2022-10-30 16:20:42 +0400

    Shugaring+adaptive

---
+++ src/js/script.js
@@ -0,0 +1,118 @@
+/* hamburger */
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active')
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active')
+});
+
+/* collapsible */
+
+let coll = document.getElementsByClassName('que__collapsible');
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px'
+        }
+    })
+};
+
+// Modal
+
+$('[data-modal=consultation]').on('click', function () {
+    $('.overlay-mod, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function () {
+    $('.overlay-mod, #consultation').fadeOut('slow');
+});
+
+$('.header__right-btn, .about__enroll-btn').each(function (i) {
+    $(this).on('click', function () {
+        $('.overlay-mod, #consultation').fadeIn('slow');
+    })
+});
+
+/* function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+$('form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+}); */
+
+// Smooth scroll and pageup
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
+
+
+/* WOW */
+
+new WOW().init();
+
+
+
+
+
+
